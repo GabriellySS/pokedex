a965db7071991bc5aa4f8a5a9eb06c3d938d442c
Gabrielly Santos <gabriellysantos@live.com>
2025-04-03 18:58:57 -0300

    adição dos detalhes dos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
+// function convertPokemonTypesToLi(pokemonTypes) {
+//     return pokemonTypes.map((typeSlop) => `<li class="type">${typeSlop.type.name}</li>`)
+// }
+
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span> 
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">Grass</li>
-                    <li class="type">Poison</li>
+                    ${pokemon.types.map((type) => `<li class="type card-${type}">${type}</li>`).join('')}
                 </ol>
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">    
+                <img src="${pokemon.photo}" alt="${pokemon.name}">    
             </div>        
         </li> 
     `   
