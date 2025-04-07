9fa7c29762b83e9a4d5e37020c1633249ccdb33d
Gabrielly Santos <gabriellysantos@live.com>
2025-04-07 20:25:20 -0300

    adição de botão de paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,62 @@
-// function convertPokemonTypesToLi(pokemonTypes) {
-//     return pokemonTypes.map((typeSlop) => `<li class="type">${typeSlop.type.name}</li>`)
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 20
+let offset = 0;
+
+// function convertPokemonToLi(pokemon) {
+//     return `
+//         <li class="pokemon ${pokemon.type}">
+//             <span class="number">#${pokemon.number}</span>
+//             <span class="name">${pokemon.name}</span> 
+
+//             <div class="detail">
+//                 <ol class="types">
+//                     ${pokemon.types.map((type) => `<li class="type card-${type}">${type}</li>`).join('')}
+//                 </ol>
+//                 <img src="${pokemon.photo}" alt="${pokemon.name}">    
+//             </div>        
+//         </li> 
+//     `   
 // }
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span> 
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span> 
 
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type card-${type}">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}" alt="${pokemon.name}">    
-            </div>        
-        </li> 
-    `   
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type card-${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">    
+                </div>        
+            </li> 
+        `).join('')
+        pokemonList.innerHTML += newHtml
+    })     
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
-}) 
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+
+    const qtdRecordNextPage = offset + limit
+
+    if (qtdRecordNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+    
+})
+
+// pokeApi.getPokemons().then((pokemons = []) => {
+//     pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+// }) 
